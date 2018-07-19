bb3521e9757fa5f133baff20fd63b1c179a81572
Chris Lewis <colshag@gmail.com>
2018-07-19 10:31:53 -0600

    finished week 6 assignment

--- week 6 optimization/optimization.py
+++ week 6 optimization/optimization.py
@@ -333,10 +333,9 @@ def initialize_velocity(parameters):
     # Initialize velocity
     for l in range(L):
         ### START CODE HERE ### (approx. 2 lines)
-        v["dW" + str(l+1)] = None
-        v["db" + str(l+1)] = None
-        ### END CODE HERE ###
-        
+        v["dW" + str(l+1)] = np.zeros((parameters['W' + str(l+1)].shape[0], parameters['W' + str(l+1)].shape[1]))
+        v["db" + str(l+1)] = np.zeros((parameters['b' + str(l+1)].shape[0], 1))
+        ### END CODE HERE ###      
     return v
 
 
@@ -429,11 +428,11 @@ def update_parameters_with_momentum(parameters, grads, v, beta, learning_rate):
         
         ### START CODE HERE ### (approx. 4 lines)
         # compute velocities
-        v["dW" + str(l+1)] = None
-        v["db" + str(l+1)] = None
+        v["dW" + str(l+1)] = beta * v["dW" + str(l+1)] + (1 - beta) * grads['dW' + str(l+1)]
+        v["db" + str(l+1)] = beta * v["db" + str(l+1)] + (1 - beta) * grads['db' + str(l+1)]
         # update parameters
-        parameters["W" + str(l+1)] = None
-        parameters["b" + str(l+1)] = None
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * v["dW" + str(l+1)]
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * v["db" + str(l+1)]
         ### END CODE HERE ###
         
     return parameters, v
@@ -598,10 +597,12 @@ def initialize_adam(parameters) :
     # Initialize v, s. Input: "parameters". Outputs: "v, s".
     for l in range(L):
     ### START CODE HERE ### (approx. 4 lines)
-        v["dW" + str(l+1)] = None
-        v["db" + str(l+1)] = None
-        s["dW" + str(l+1)] = None
-        s["db" + str(l+1)] = None
+        WZeros = np.zeros((parameters['W' + str(l+1)].shape[0], parameters['W' + str(l+1)].shape[1]))
+        bZeros = np.zeros((parameters['b' + str(l+1)].shape[0], 1))      
+        v["dW" + str(l+1)] = WZeros
+        v["db" + str(l+1)] = bZeros
+        s["dW" + str(l+1)] = WZeros
+        s["db" + str(l+1)] = bZeros
     ### END CODE HERE ###
     
     return v, s
@@ -729,32 +730,32 @@ def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate = 0.01
     for l in range(L):
         # Moving average of the gradients. Inputs: "v, grads, beta1". Output: "v".
         ### START CODE HERE ### (approx. 2 lines)
-        v["dW" + str(l+1)] = None
-        v["db" + str(l+1)] = None
+        v["dW" + str(l+1)] = beta1 * v["dW" + str(l+1)] + (1. - beta1) * grads['dW' + str(l+1)]
+        v["db" + str(l+1)] = beta1 * v["db" + str(l+1)] + (1. - beta1) * grads['db' + str(l+1)]
         ### END CODE HERE ###
 
         # Compute bias-corrected first moment estimate. Inputs: "v, beta1, t". Output: "v_corrected".
         ### START CODE HERE ### (approx. 2 lines)
-        v_corrected["dW" + str(l+1)] = None
-        v_corrected["db" + str(l+1)] = None
+        v_corrected["dW" + str(l+1)] = v["dW" + str(l+1)] / (1. - beta1 ** t)
+        v_corrected["db" + str(l+1)] = v["db" + str(l+1)] / (1. - beta1 ** t)
         ### END CODE HERE ###
 
         # Moving average of the squared gradients. Inputs: "s, grads, beta2". Output: "s".
         ### START CODE HERE ### (approx. 2 lines)
-        s["dW" + str(l+1)] = None
-        s["db" + str(l+1)] = None
+        s["dW" + str(l+1)] = beta2 * s["dW" + str(l+1)] + (1. - beta2) * (grads['dW' + str(l+1)] ** 2)
+        s["db" + str(l+1)] = beta2 * s["db" + str(l+1)] + (1. - beta2) * (grads['db' + str(l+1)] ** 2)
         ### END CODE HERE ###
 
         # Compute bias-corrected second raw moment estimate. Inputs: "s, beta2, t". Output: "s_corrected".
         ### START CODE HERE ### (approx. 2 lines)
-        s_corrected["dW" + str(l+1)] = None
-        s_corrected["db" + str(l+1)] = None
+        s_corrected["dW" + str(l+1)] = s["dW" + str(l+1)] / (1. - beta2 ** t)
+        s_corrected["db" + str(l+1)] = s["db" + str(l+1)] / (1. - beta2 ** t)
         ### END CODE HERE ###
 
         # Update parameters. Inputs: "parameters, learning_rate, v_corrected, s_corrected, epsilon". Output: "parameters".
         ### START CODE HERE ### (approx. 2 lines)
-        parameters["W" + str(l+1)] = None
-        parameters["b" + str(l+1)] = None
+        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * v_corrected["dW" + str(l+1)] / (np.sqrt(s_corrected["dW" + str(l+1)]) + epsilon)
+        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * v_corrected["db" + str(l+1)] / (np.sqrt(s_corrected["db" + str(l+1)]) + epsilon)
         ### END CODE HERE ###
 
     return parameters, v, s
